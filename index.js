b13a6c7a0f8c65d3278cc321fb9ecafa36698024
Mario776612 <Mario776612tak@gmail.com>
2024-03-02 20:37:39 +0100

    Zmiany

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const player = new THREE.Mesh( geometry, material );
+scene.add( player );
+
+player.position.y = -3;
+camera.position.z = 5;
+
+let PlayerMove = () => {
+    window.addEventListener('keydown', function (event) {
+        if (event.key === 'a' || event.key === 'A') {
+        player.position.x -= 1;
+      } else if (event.key === 'd' || event.key === 'D') {
+        player.position.x += 1;
+      }
+    });
+  };
+
+function animate() {
+	requestAnimationFrame( animate );
+
+
+	renderer.render( scene, camera );
+}
+PlayerMove();
+animate();
