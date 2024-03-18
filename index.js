9d4b99c080387f36a9c79cfc4f9045519fa3758b
Mario776612 <Mario776612tak@gmail.com>
2024-03-18 20:46:19 +0100

    a (troche czyszczenia syfu w kodzie i dodanie hitboxow +/-)

--- index.js
+++ index.js
@@ -14,56 +14,79 @@ controls.update();
 let CreateBoxGeometry = (w , h , d) => {
   return new THREE.BoxGeometry(w,h,d)
 }
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const player = new THREE.Mesh(CreateBoxGeometry(1,1,1), material );
-scene.add( player );
-scene.updateMatrixWorld(true)
-var PlayerPosition = new THREE.Vector3()
-PlayerPosition.setFromMatrixPosition( player.matrixWorld)
-
-player.position.y = -3;
 camera.position.z = 5;
-
-let ActiveBullets = [];
-
+//Background
 const geometry = new THREE.CylinderGeometry( 5, 5, 20, 8 ); 
 const mat = new THREE.MeshBasicMaterial({wireframe: true, color:0x00ff00 })
 const cylinder = new THREE.Mesh( geometry,mat ); 
 cylinder.rotation.x = 80;
 cylinder.position.z = 10;
 scene.add( cylinder );
+//Shooting + Player
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const player = new THREE.Mesh(CreateBoxGeometry(1,1,1), material );
+scene.add( player );
+player.position.y = -3;
+scene.updateMatrixWorld(true)
 
+let PlayerBox = new THREE.Box3(new THREE.Vector3(),new THREE.Vector3())
+PlayerBox.setFromObject(player)
+let ActiveBullets = [];
 let Shoot = () => {
   const bullet = new THREE.Mesh( CreateBoxGeometry(0.1,0.1,0.1), material );
   scene.add( bullet )
   bullet.position.set(player.position.x,player.position.y+0.7,player.position.z)
   ActiveBullets.push(bullet)
 }
+
 let PlayerMove = () => {
     window.addEventListener('keydown', function (event) {
         if (event.key === 'a' || event.key === 'A') {
         player.position.x -= 0.5;
       } else if (event.key === 'd' || event.key === 'D') {
         player.position.x += 0.5;
+      } else if (event.key === 'w' || event.key === 'W') {
+        player.position.y += 0.5;
+      } else if (event.key === 's' || event.key === 'S') {
+        player.position.y -= 0.5;
       } else if (event.key === 'e' || event.key === 'E') {
         Shoot();
       }
     });
   };
-var EnemyPositionCap = new THREE.Vector3(4,3,0)
+//Enemy *Make this shit as function later*
+//var EnemyPositionCap = new THREE.Vector3(4,3,0)
 const enemy = new THREE.Mesh(CreateBoxGeometry(1,1,1), material );
+const enemyBox = new THREE.Box3(new THREE.Vector3() , new THREE.Vector3());
+enemyBox.setFromObject(enemy)
 enemy.position.y = 3;
-scene.add(enemy)
-let flip = true;
 
+scene.add(enemy)
+
+//Collision
+let checkCollision = () => {
+if(PlayerBox.intersectsBox(enemyBox)){
+  console.log(true)
+}
+}
+
+//let flip = true;
 function animate() {
   requestAnimationFrame( animate );
+  enemyBox.copy(enemy.geometry.boundingBox).applyMatrix4(enemy.matrixWorld);
+  PlayerBox.copy(player.geometry.boundingBox).applyMatrix4(player.matrixWorld);
+
+
+
+
+// Clean this shit up
   for(let i in ActiveBullets){
     ActiveBullets[i].position.y += 0.2
     if(ActiveBullets[i].position.y >= 4){
       scene.remove( ActiveBullets[i] );
     }
   }
+  /*
   if(flip == true){
     enemy.position.x += 0.02;
     if (enemy.position.x > EnemyPositionCap.x) {
@@ -75,6 +98,7 @@ function animate() {
       flip = true;
     }
   }
+  */
   cylinder.rotation.y += 0.02;
   controls.update();
   renderer.render( scene, camera );
